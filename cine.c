2929f91db28120e6620a78336ab9c46e342a636d
LuisLV <65624337+Luis-LV@users.noreply.github.com>
2020-05-31 00:59:43 -0500

    Add files via upload

---
+++ cine.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    int opcion;
+    
+
+      while(opcion!=11){
+            printf("\nCINETOTAL\n");
+            printf("\n===Menu===");
+            printf("\n 1.CARTELERA");
+            printf("\n 2.ADQUIRIR TARJETA");
+            printf("\n 3.COMPRAR BOLETOS");
+            printf("\n 4.CANJEAR PUNTOS A BOLETOS");
+            printf("\n 5.CANJEAR PUNTOS A DULCERIA");
+            printf("\n 6.DULCERIA");
+            printf("\n 7.COMPRAR PARA PELICULAS PROXIMAS");
+            printf("\n 8.CLIENTE VIP");
+            printf("\n 9.QUEJAS");
+            printf("\n 10.OPINION");
+            printf("\n 11.Salir");
+            printf("\n SU OPCION: ");
+            scanf("%d", &opcion );
+            
+              
+              
+              if(opcion==1){
+                  printf("\n \n");
+                  
+              }  
+                  if(opcion==2){
+                      printf("\n \n");
+                      
+                  }
+                  if(opcion==3){
+                      printf("\n \n");
+                      
+                  }
+                  if(opcion==4){
+                      printf("\n \n");
+                  
+                   }
+                   if(opcion==5){
+                       printf("\n \n");
+                         
+                   }
+                   if(opcion==6){
+                       printf("\n \n");
+                         
+                   }
+                   if(opcion==7){
+                       printf("\n \n");
+                         
+                   }
+                   if(opcion==8){
+                       printf("\n \n");
+                         
+                   }
+                   if(opcion==9){
+                       printf("\n \n");
+                         
+                   }
+                   if(opcion==10){
+                       printf("\n \n");
+                         
+                   }
+                   if(opcion==11){
+                       printf("\n \n");
+                         
+                   }
+                   
+      }
+
+    return 0;
+}
